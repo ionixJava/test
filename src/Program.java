96a4788c322e3099d951298d7e44373654211842
Mehmet Goren <mehmetgoren@outlook.com>
2016-02-08 11:31:49 +0200

    1

--- src/Program.java
+++ src/Program.java
@@ -11,11 +11,18 @@ public final class Program {
                 System.out.println("Complete: " + e.getQuery().toString() + " - " + e.isSucceeded());
             });
 
-            SqlQuery q = SqlQuery.toQuery("UPDATE SICIL SET ADI=@Name WHERE SICILK=@Id")
-                    .paramater("Name", "Cihat")
-                    .paramater("Id", 9372);
+          //  SqlQuery q = SqlQuery.toQuery("UPDATE SICIL SET ADI=@Name WHERE SICILK=@Id")
+                //    .paramater("Name", "Cihat")
+                 //   .paramater("Id", 9372);
 
-            System.out.println(dataAccess.executeUpdate(q));
+          //  System.out.println(dataAccess.executeUpdate(q));
+
+            SqlQuery q = SqlQuery.toQuery("select SICILK FROM SICIL WHERE SICILNO=?")
+                    .paramater(1,"TP.0891");
+
+            Object val = dataAccess.executeScalar(q, Integer.class);
+
+            System.out.println(val + " - " + val.getClass().getName());
 
             dataAccess.commit();
         }
