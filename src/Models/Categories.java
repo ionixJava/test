d892687fae142c0a97358f158496a43520695f81
Mehmet Goren <mehmetgoren@outlook.com>
2016-02-10 16:02:24 +0200

    from tokyo

---
+++ src/Models/Categories.java
@@ -0,0 +1,33 @@
+package Models;
+
+import ionix.Annotation.DbSchema;
+import ionix.Annotation.Table;
+import ionix.Data.StoreGeneratedPattern;
+
+
+@Table(schema = "NORTHWND.DBO",name="Categories")
+public class Categories {
+    @DbSchema(columnName = "CategoryID", isKey = true,databaseGeneratedOption = StoreGeneratedPattern.Identity)
+    private int categoryID;
+    public int getCategoryID(){ return this.categoryID;}
+    public Categories setCategoryID(int value){
+        this.categoryID = value;
+        return this;
+    }
+
+    @DbSchema(columnName = "CategoryName")
+    private String categoryName;
+    public String getCategoryName(){ return this.categoryName;}
+    public Categories setCategoryName(String value){
+        this.categoryName = value;
+        return this;
+    }
+
+    @DbSchema(columnName = "Description")
+    private String description;
+    public String getDescription(){ return this.description;}
+    public Categories setDescription(String value){
+        this.description = value;
+        return this;
+    }
+}
--- src/Program.java
+++ src/Program.java
@@ -1,28 +1,64 @@
 import java.math.BigDecimal;
 import java.sql.Connection;
+
+import Models.Categories;
 import ionix.Data.*;
+import ionix.Utils.Ser;
 
 public final class Program {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        testSelectSingle();
+    }
+
+    public static void testTransaction() {
         Connection conn = DB.createConnection();
-        try(TransactionalDbAccess dataAccess = new TransactionalDbAccess(conn)) {
+        try (TransactionalDbAccess dataAccess = new TransactionalDbAccess(conn)) {
             dataAccess.onExecuteSqlComplete((e) -> {
                 System.out.println("Complete: " + e.getQuery().toString() + " - " + e.isSucceeded());
             });
 
-          //  SqlQuery q = SqlQuery.toQuery("UPDATE SICIL SET ADI=@Name WHERE SICILK=@Id")
-                //    .paramater("Name", "Cihat")
-                 //   .paramater("Id", 9372);
+            SqlQuery q = SqlQuery.toQuery("UPDATE SICIL SET ADI=@Name WHERE SICILK=@Id")
+                    .paramater("Name", "Cihat")
+                    .paramater("Id", 9372);
 
-          //  System.out.println(dataAccess.executeUpdate(q));
+            System.out.println(dataAccess.executeUpdate(q));
 
-            SqlQuery q = SqlQuery.toQuery("select SICILK FROM SICIL WHERE SICILNO=?")
-                    .paramater(1,"TP.0891");
 
-            Object val = dataAccess.executeScalar(q, Integer.class);
+            dataAccess.commit();
+        }
+    }
 
-            System.out.println(val + " - " + val.getClass().getName());
+    public static void testQuerySingle(){
+        Connection conn = DB.createConnection();
+        try (DbAccess dataAccess = new TransactionalDbAccess(conn)) {
+
+
+
+            EntityCommandSelect cmd = new EntityCommandSelect(dataAccess).setConvertType(true);
+            final EntityMetaDataProvider provider = new DbSchemaMetaDataProvider();
+
+            Categories entity = cmd.querySingle(Categories.class, provider, SqlQuery.toQuery("select * from NORTHWND.dbo.Categories where CategoryID=7"));
+
+            System.out.println(Ser.toJson(entity));
+
+        }
+    }
+
+    public static void testSelectSingle(){
+        Connection conn = DB.createConnection();
+        try (TransactionalDbAccess dataAccess = new TransactionalDbAccess(conn)) {
+            dataAccess.onExecuteSqlComplete((e) -> {
+                System.out.println("Complete: " + e.getQuery().toString() + " - " + e.isSucceeded());
+            });
+
+
+            EntityCommandSelect cmd = new EntityCommandSelect(dataAccess);
+            final EntityMetaDataProvider provider = new DbSchemaMetaDataProvider();
+
+            Categories entity = cmd.selectSingle(Categories.class, provider, SqlQuery.toQuery(" where CategoryID=?",7));
+
+            System.out.println(Ser.toJson(entity));
 
             dataAccess.commit();
         }
