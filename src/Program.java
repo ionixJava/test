cc5008e34156a4dcd335e0d2398695209c9d9c20
Mehmet Goren <mehmetgoren@outlook.com>
2016-02-19 19:52:02 +0200

    1

--- src/Program.java
+++ src/Program.java
@@ -10,20 +10,11 @@ import ionix.Utils.Ser;
 public final class Program {
 
     static final int length = 1000;
+
     public static void main(String[] args) {
-
-        testQuery();
-
-        testQuery();
-
-        testSelect();
-
-        testQuery();
-
-      //  testQuerySingle();
-
-      //  testQuerySingle();
-       // testSelectSingle();
+        testFilterCriteriaTest();
+        System.out.println("\n");
+        testSelectById();
     }
 
     public static void testTransaction() {
@@ -44,52 +35,29 @@ public final class Program {
         }
     }
 
-    public static void testQuerySingle(){
+    public static void testQuerySingle() {
         Connection conn = DB.createConnection();
         try (DbAccess dataAccess = new TransactionalDbAccess(conn)) {
-
 
 
             EntityCommandSelect cmd = new EntityCommandSelect(dataAccess);//.setConvertType(true);
             final EntityMetaDataProvider provider = new DbSchemaMetaDataProvider();
 
-           // String sql = "select CategoryID,CategoryName,Description from NORTHWND.dbo.Categories where CategoryID=?";
+            // String sql = "select CategoryID,CategoryName,Description from NORTHWND.dbo.Categories where CategoryID=?";
             String sql = "select * from NORTHWND.dbo.Categories where CategoryID=?";
 
             Date start = new Date();
-            for(int j = 0; j < length; ++j){
-            Categories entity = cmd.querySingle(Categories.class, provider, SqlQuery.toQuery(sql, 7));
+            for (int j = 0; j < length; ++j) {
+                Categories entity = cmd.querySingle(Categories.class, provider, SqlQuery.toQuery(sql, 7));
             }
             Date end = new Date();
             System.out.println((end.getTime() - start.getTime()));
 
-           // System.out.println(Ser.toJson(entity));
+            // System.out.println(Ser.toJson(entity));
         }
     }
 
-    public static void testSelectSingle(){
-        Connection conn = DB.createConnection();
-        try (DbAccess dataAccess = new TransactionalDbAccess(conn)) {
-            dataAccess.onExecuteSqlComplete((e) -> {
-               // System.out.println("Complete: " + e.getQuery().toString() + " - " + e.isSucceeded());
-            });
-
-
-            EntityCommandSelect cmd = new EntityCommandSelect(dataAccess);//.setConvertType(true);
-            final EntityMetaDataProvider provider = new DbSchemaMetaDataProvider();
-
-            Date start = new Date();
-            for(int j = 0; j < length; ++j){
-                Categories entity = cmd.selectSingle(Categories.class, provider, SqlQuery.toQuery(" where CategoryID=?",7));
-            }
-            Date end = new Date();
-            System.out.println((end.getTime() - start.getTime()));
-
-            //System.out.println(Ser.toJson(entity));
-        }
-    }
-
-    public static void testSelect(){
+    public static void testSelectSingle() {
         Connection conn = DB.createConnection();
         try (DbAccess dataAccess = new TransactionalDbAccess(conn)) {
             dataAccess.onExecuteSqlComplete((e) -> {
@@ -101,7 +69,29 @@ public final class Program {
             final EntityMetaDataProvider provider = new DbSchemaMetaDataProvider();
 
             Date start = new Date();
-            for(int j = 0; j < length; ++j){
+            for (int j = 0; j < length; ++j) {
+                Categories entity = cmd.selectSingle(Categories.class, provider, SqlQuery.toQuery(" where CategoryID=?", 7));
+            }
+            Date end = new Date();
+            System.out.println((end.getTime() - start.getTime()));
+
+            //System.out.println(Ser.toJson(entity));
+        }
+    }
+
+    public static void testSelect() {
+        Connection conn = DB.createConnection();
+        try (DbAccess dataAccess = new TransactionalDbAccess(conn)) {
+            dataAccess.onExecuteSqlComplete((e) -> {
+                // System.out.println("Complete: " + e.getQuery().toString() + " - " + e.isSucceeded());
+            });
+
+
+            EntityCommandSelect cmd = new EntityCommandSelect(dataAccess);//.setConvertType(true);
+            final EntityMetaDataProvider provider = new DbSchemaMetaDataProvider();
+
+            Date start = new Date();
+            for (int j = 0; j < length; ++j) {
                 List<Categories> entityList = cmd.select(Categories.class, provider, null);
             }
             Date end = new Date();
@@ -111,7 +101,7 @@ public final class Program {
         }
     }
 
-    public static void testQuery(){
+    public static void testQuery() {
         Connection conn = DB.createConnection();
         try (DbAccess dataAccess = new TransactionalDbAccess(conn)) {
             dataAccess.onExecuteSqlComplete((e) -> {
@@ -122,15 +112,54 @@ public final class Program {
             EntityCommandSelect cmd = new EntityCommandSelect(dataAccess);//.setConvertType(true);
             final EntityMetaDataProvider provider = new DbSchemaMetaDataProvider();
 
-             Date start = new Date();
-             for(int j = 0; j < length; ++j){
-            List<Categories> entityList = cmd.query(Categories.class, provider, SqlQuery.toQuery("select * from NORTHWND.dbo.Categories"));
-              }
-             Date end = new Date();
-             System.out.println((end.getTime() - start.getTime()));
+            Date start = new Date();
+            for (int j = 0; j < length; ++j) {
+                List<Categories> entityList = cmd.query(Categories.class, provider, SqlQuery.toQuery("select * from NORTHWND.dbo.Categories"));
+            }
+            Date end = new Date();
+            System.out.println((end.getTime() - start.getTime()));
 
             //System.out.println(Ser.toJson(entityList));
         }
     }
 
+    public static void testSelectById() {
+        Connection conn = DB.createConnection();
+        try (DbAccess dataAccess = new TransactionalDbAccess(conn)) {
+            dataAccess.onExecuteSqlComplete((e) -> {
+                // System.out.println("Complete: " + e.getQuery().toString() + " - " + e.isSucceeded());
+            });
+
+            EntityCommandSelect cmd = new EntityCommandSelect(dataAccess);//.setConvertType(true);
+            final EntityMetaDataProvider provider = new DbSchemaMetaDataProvider();
+
+            Categories entity = cmd.selectById(Categories.class, provider, 8);
+
+            System.out.println(Ser.toJson(entity));
+        }
+    }
+
+    public static void testFilterCriteriaTest() {
+        Connection conn = DB.createConnection();
+        try (DbAccess dataAccess = new TransactionalDbAccess(conn)) {
+            dataAccess.onExecuteSqlComplete((e) -> {
+                System.out.println("Complete: " + e.getQuery().toString() + " - " + e.isSucceeded());
+            });
+
+            final Class entiyClass = Categories.class;
+            final EntityMetaDataProvider provider = new DbSchemaMetaDataProvider();
+
+            FilterCriteriaList filters = new FilterCriteriaList()
+                    .add("CategoryID", ConditionOperator.In, "1", 2, 3, 4, 5)
+                    .add("CategoryName", ConditionOperator.StartsWith, "Co");
+
+            SqlQuery query = new SqlQueryProviderSelect(provider.createEntityMetaData(entiyClass)).toQuery()
+                    .combine(filters.toQuery());
+            EntityCommandSelect cmd = new EntityCommandSelect(dataAccess);//.setConvertType(true);
+            List<Categories> entityList = cmd.query(entiyClass, provider, query);
+
+            System.out.println();
+            System.out.println(Ser.toJson(entityList));
+        }
+    }
 }
